da0fd1c1c4386be25e37210c6fe9fef7fa8664f8
chcodee <you@example.com>
2023-04-17 10:28:45 +0530

    simple snake game

---
+++ simple snake game.py
@@ -0,0 +1,138 @@
+import turtle
+import time
+import random
+import tkinter as tk
+
+delay = 0.1
+
+# Set up the screen
+screen = turtle.Screen()
+screen.title("Snake Game")
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+
+# Create the snake's head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Create the snake's food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+segments = []
+
+# Functions to move the snake
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    elif head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    elif head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+    elif head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+# Functions to change the direction of the snake
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+# Keyboard bindings
+screen.listen()
+screen.onkeypress(go_up, "Up")
+screen.onkeypress(go_down, "Down")
+screen.onkeypress(go_right, "Right")
+screen.onkeypress(go_left, "Left")
+
+# Main game loop
+while True:
+    screen.update()
+
+    # Check for collision with border
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+         # Game over popup
+        root = tk.Tk()
+        root.withdraw()
+        tk.messagebox.showinfo("Game Over", "You hit the wall! Try again.")
+        root.destroy()
+
+    # Check for collision with food
+    if head.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        
+      
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    move()
+
+    # Check for collision with body
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segments.clear()
+            # Game over popup
+            root = tk.Tk()
+            root.withdraw()
+            tk.messagebox.showinfo("Game Over", "You hit the Your! Try again.")
+            root.destroy()
+
+            
+
+    time.sleep(delay)
+
+turtle.done()
